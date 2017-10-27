63c04bde85d52c374c0aa59d8e31a30cc3636f2c
Valeriy Saphonov <xxrrrxx@yandex.ru>
2017-10-27 13:28:52 +0300

    update SDK docs - 3

--- index.android.js
+++ index.android.js
@@ -89,10 +89,10 @@ function VoxImplantSDK () {
   };
     /**
      * Create new call
-     * @param to - SIP URI, username or phone number to make call to. Actual routing is then performed by VoxEngine scenario
-     * @param video - Enable video support in call
-     * @param customData - Optional custom data passed with call. Will be available in VoxEngine scenario
-     * @param callback - Callback object
+     * @param {string} to - SIP URI, username or phone number to make call to. Actual routing is then performed by VoxEngine scenario
+     * @param {boolean} video - Enable video support in call
+     * @param {string} customData - Optional custom data passed with call. Will be available in VoxEngine scenario
+     * @param {object} callback - Callback object
      */
   this.createCall = function(to, video, customData, callback) {
     if (typeof(video) == 'function') {
@@ -117,39 +117,39 @@ function VoxImplantSDK () {
   };
     /**
      * Login to specified VoxImplant application
-     * @param user - Full user name, including app and account name, like <i>someuser@someapp.youraccount.voximplant.com</i>
-     * @param password - User password
+     * @param {string} user - Full user name, including app and account name, like <i>someuser@someapp.youraccount.voximplant.com</i>
+     * @param {string} password - User password
      */
   this.login = function(user, password) {
     VoxImplantModule.login(user, password);
   };
     /**
      * Perform login using one time key that was generated before
-     * @param user - Full user name, including app and account name, like <i>someuser@someapp.youraccount.voximplant.com</i>
-     * @param hash - Hash that was generated using following formula: MD5(oneTimeKey+"|"+MD5(user+":voximplant.com:"+password)). <b>Please note that here user is just a user name, without app name, account name or anything else after "@"</b>. So if you pass <i>myuser@myapp.myacc.voximplant.com</i> as a<b>username</b>, you should only use <i>myuser</i>  while computing this hash.
+     * @param {string} user - Full user name, including app and account name, like <i>someuser@someapp.youraccount.voximplant.com</i>
+     * @param {string} hash - Hash that was generated using following formula: MD5(oneTimeKey+"|"+MD5(user+":voximplant.com:"+password)). <b>Please note that here user is just a user name, without app name, account name or anything else after "@"</b>. So if you pass <i>myuser@myapp.myacc.voximplant.com</i> as a<b>username</b>, you should only use <i>myuser</i>  while computing this hash.
      */
   this.loginUsingOneTimeKey = function(user, hash) {
     VoxImplantModule.loginUsingOneTimeKey(user, hash);
   };
     /**
      * Perform login using specified username and access token that was obtained in onLoginSuccessful callback before
-     * @param user - Full user name, including app and account name, like <i>someuser@someapp.youraccount.voximplant.com</i>
-     * @param accessToken - access token that was obtained in onLoginSuccessful callback
+     * @param {string} user - Full user name, including app and account name, like <i>someuser@someapp.youraccount.voximplant.com</i>
+     * @param {string} accessToken - access token that was obtained in onLoginSuccessful callback
      */
   this.loginUsingAccessToken = function(user, accessToken) {
     VoxImplantModule.loginUsingAccessToken(user, accessToken);
   };
     /**
      * Perform refresh of login tokens required for login using access token
-     * @param user - Full user name, including app and account name, like <i>someuser@someapp.youraccount.voximplant.com</i>
-     * @param refreshToken - refresh token that was obtained in onLoginSuccessful callback
+     * @param {string} user - Full user name, including app and account name, like <i>someuser@someapp.youraccount.voximplant.com</i>
+     * @param {string} refreshToken - refresh token that was obtained in onLoginSuccessful callback
      */
   this.refreshToken = function(user, refreshToken) {
     VoxImplantModule.refreshToken(user, refreshToken);
   };
     /**
      * Generates one time login key to be used for automated login process.
-     * @param user - Full user name, including app and account name, like <i>someuser@someapp.youraccount.voximplant.com</i>
+     * @param {string} user - Full user name, including app and account name, like <i>someuser@someapp.youraccount.voximplant.com</i>
      */
   this.requestOneTimeKey = function(user) {
     VoxImplantModule.requestOneTimeKey(user);
@@ -162,122 +162,122 @@ function VoxImplantSDK () {
   };
     /**
      * Send start call request If call with specified id is not found - returns false
-     * @param callId - id of previously created call
-     * @param headers - Optional set of headers to be sent with message. Names must begin with "X-" to be processed by SDK
+     * @param {string} callId - id of previously created call
+     * @param {object} headers - Optional set of headers to be sent with message. Names must begin with "X-" to be processed by SDK
      */
   this.startCall = function(callId, headers) {
     VoxImplantModule.startCall(callId, headers == undefined ? {} :  headers);
   };
     /**
      * Sends DTMF digit in specified call.
-     * @param callId - id of previously created call
-     * @param digit - Digit can be 0-9 for 0-9, 10 for * and 11 for #
+     * @param {string} callId - id of previously created call
+     * @param {number} digit - Digit can be 0-9 for 0-9, 10 for * and 11 for #
      */
   this.sendDTMF = function(callId, digit) {
     VoxImplantModule.sendDTMF(callId, digit);
   };
     /**
      * Terminate specified call. Call must be either established, or outgoing progressing
-     * @param callId - id of previously created call
-     * @param headers - Optional set of headers to be sent with message. Names must begin with "X-" to be processed by Voximplant
+     * @param {string} callId - id of previously created call
+     * @param {object} headers - Optional set of headers to be sent with message. Names must begin with "X-" to be processed by Voximplant
      */
   this.disconnectCall = function(callId, headers) {
     VoxImplantModule.disconnectCall(callId, headers == undefined ? {} :  headers);
   };
     /**
      * Reject incoming alerting call
-     * @param callId - id of previously created call
-     * @param headers - Optional set of headers to be sent with message. Names must begin with "X-" to be processed by SDK
+     * @param {string} callId - id of previously created call
+     * @param {object} headers - Optional set of headers to be sent with message. Names must begin with "X-" to be processed by SDK
      */
   this.declineCall = function(callId, headers) {
     VoxImplantModule.declineCall(callId, headers == undefined ? {} :  headers);
   };
     /**
      * Answer incoming call
-     * @param callId - Id of previously created call
-     * @param headers - Optional set of headers to be sent with message. Names must begin with "X-" to be processed by SDK
+     * @param {string} callId - Id of previously created call
+     * @param {object} headers - Optional set of headers to be sent with message. Names must begin with "X-" to be processed by SDK
      */
   this.answerCall = function(callId, headers) {
     VoxImplantModule.answerCall(callId, headers == undefined ? {} :  headers);
   };
     /**
      * Sends instant message within established call
-     * @param callId - id of previously created call
-     * @param text - Message text
+     * @param {string} callId - id of previously created call
+     * @param {string} text - Message text
      */
   this.sendMessage = function(callId, text) {
     VoxImplantModule.sendMessage(callId, text);
   };
     /**
      * Sends info within established call
-     * @param callId - id of previously created call
-     * @param mimeType - MIME type of info
-     * @param content - Custom string data
-     * @param headers - Optional set of headers to be sent with message. Names must begin with "X-" to be processed by SDK
+     * @param {string} callId - id of previously created call
+     * @param {string} mimeType - MIME type of info
+     * @param {string} content - Custom string data
+     * @param {object} headers - Optional set of headers to be sent with message. Names must begin with "X-" to be processed by SDK
      */
   this.sendInfo = function(callId, mimeType, content, headers) {
     VoxImplantModule.sendInfo(callId, mimeType, content, headers == undefined ? {} : headers);
   };
     /**
      * Mute or unmute microphone. This is reset after audio interruption
-     * @param doMute - Enable/disable flag
+     * @param {boolean} doMute - Enable/disable flag
      */
   this.setMute = function(doMute) {
     VoxImplantModule.setMute(doMute);
   };
     /**
      * Enable/disable loudspeaker
-     * @param useLoudSpeaker - Enable/disable loudspeaker
+     * @param {boolean} useLoudSpeaker - Enable/disable loudspeaker
      */
   this.setUseLoudspeaker = function(useLoudSpeaker) {
     VoxImplantModule.setUseLoudspeaker(useLoudSpeaker);
   };
     /**
      * Set video display mode. Applies to both incoming and outgoing stream. IOS ONLY
-     * @param mode - Resize mode
+     * @param {string} mode - Resize mode
      */
   this.setVideoResizeMode = function(mode) {
     VoxImplantModule.setVideoResizeMode(mode);
   };
     /**
      * Start/stop sending video from local camera
-     * @param doSendVideo - Specify if video should be sent
+     * @param {boolean} doSendVideo - Specify if video should be sent
      */
   this.sendVideo = function (doSendVideo) {
     VoxImplantModule.sendVideo(doSendVideo);
   };
     /**
      * Set local camera resolution
-     * @param width - camera resolution width
-     * @param height - camera resolution height
+     * @param {number} width - camera resolution width
+     * @param {number} height - camera resolution height
      */
   this.setCameraResolution = function(width, height) {
     VoxImplantModule.setCameraResolution(width, height);
   };
     /**
      * Switch camera
-     * @param cameraName - Must be "front" or "back"
+     * @param {string} cameraName - Must be "front" or "back"
      */
   this.switchToCamera = function(cameraName) {
     VoxImplantModule.switchToCamera(cameraName);
   };
     /**
      * Register for push notifications. Application will receive push notifications from VoxImplant Server after first log in.
-     * @param pushRegistrationToken - Push registration token
+     * @param {string} pushRegistrationToken - Push registration token
      */
   this.registerForPushNotifications = function(pushRegistrationToken) {
     VoxImplantModule.registerForPushNotifications(pushRegistrationToken);
   };
     /**
      * Unregister from push notifications. Application will no longer receive push notifications from VoxImplant server
-     * @param pushRegistrationToken - Push registration token that was used to register for push notifications
+     * @param {string} pushRegistrationToken - Push registration token that was used to register for push notifications
      */
   this.unregisterFromPushNotifications = function(pushRegistrationToken) {
     VoxImplantModule.unregisterFromPushNotifications(pushRegistrationToken);
   };
     /**
      * Handle incoming push notification
-     * @param notification - Incoming push notification
+     * @param {object} notification - Incoming push notification
      */
   this.handlePushNotification = function(notification) {
     VoxImplantModule.handlePushNotification(notification);
@@ -286,18 +286,18 @@ function VoxImplantSDK () {
     this.Events = {
         /**
          * Invoked when login process finished successfully.
-         * @property displayName - Display name of logged in user
-         * @property loginTokens - Login tokens that can be used to login using access token
+         * @property {string} displayName - Display name of logged in user
+         * @property {object} loginTokens - Login tokens that can be used to login using access token
          */
         LoginSuccessful: "LoginSuccessful",
         /**
          * Invoked when login process failed
-         * @property reason - Failure reason
+         * @property {string} reason - Failure reason
          */
         LoginFailed: "LoginFailed",
         /**
          * Returns one time key generated by the login server as a result of requestOneTimeLoginKey.
-         * @property key - One time key
+         * @property {string} key - One time key
          */
         OneTimeKeyGenerated: "OneTimeKeyGenerated",
         /**
@@ -310,80 +310,80 @@ function VoxImplantSDK () {
         ConnectionClosed: "ConnectionClosed",
         /**
          * Connection with cloud failed
-         * @property reason - Error message
+         * @property {string} reason - Error message
          */
         ConnectionFailed: "ConnectionFailed",
         /**
          * Call established
-         * @property callId - id of call
-         * @property headers - Optional headers passed with event
+         * @property {string} callId - id of call
+         * @property {object} headers - Optional headers passed with event
          */
         CallConnected: "CallConnected",
         /**
          * Call terminated
-         * @property callId - id of call
-         * @property headers - Optional headers passed with event
-         * @property answeredElsewhere - Indicate if the call was answered on other peer
+         * @property {string} callId - id of call
+         * @property {object} headers - Optional headers passed with event
+         * @property {boolean} answeredElsewhere - Indicate if the call was answered on other peer
          */
         CallDisconnected: "CallDisconnected",
         /**
          * Call ringing. You should start playing call progress tone now
-         * @property callId - id of call
-         * @property headers - Optional headers passed with event
+         * @property {string} callId - id of call
+         * @property {object} headers - Optional headers passed with event
          */
         CallRinging: "CallRinging",
         /**
          * Outgoing call failed
-         * @property callId - id of call
-         * @property code - Status code
-         * @property reason - Status message
-         * @property headers - Optional headers passed with event
+         * @property {string} callId - id of call
+         * @property {number} code - Status code
+         * @property {string} reason - Status message
+         * @property {object} headers - Optional headers passed with event
          */
         CallFailed: "CallFailed",
         /**
          * Call audio started. You should stop playing progress tone when event is received
-         * @property callId - id of call
+         * @property {string} callId - id of call
          */
         CallAudioStarted: "CallAudioStarted",
         /**
          * New incoming call received by SDK
-         * @property callId - id of call
-         * @property from - SIP URI of caller
-         * @property displayName - Displayed name of caller
-         * @property videoCall - video call flag
-         * @property headers - Optional headers passed with event
+         * @property {string} callId - id of call
+         * @property {string} from - SIP URI of caller
+         * @property {string} displayName - Displayed name of caller
+         * @property {boolean} videoCall - video call flag
+         * @property {object} headers - Optional headers passed with event
          */
         IncomingCall: "IncomingCall",
         /**
          * SIP INFO received during a call
-         * @property callId - id of call
-         * @property type - MIME type of info
-         * @property content - Body of info message
-         * @property headers - Optional headers passed with event
+         * @property {string} callId - id of call
+         * @property {string} type - MIME type of info
+         * @property {string} content - Body of info message
+         * @property {object} headers - Optional headers passed with event
          */
         SIPInfoReceivedInCall: "SIPInfoReceivedInCall",
         /**
          * Instant message received during a call
-         * @property callId - id of call
-         * @property text - Message text
+         * @property {string} callId - id of call
+         * @property {string} text - Message text
          */
         MessageReceivedInCall: "MessageReceivedInCall",
         /**
          * Event dispatched when packet loss data received from VoxImplant servers
-         * @property callId - id of call
-         * @property stats - NetworkInfo
+         * @property {string} callId - id of call
+         * @property {object} stats - NetworkInfo
          */
         NetStatsReceived:"NetStatsReceived",
         /**
          * Invoked when refresh of login tokens finished successfully
-         * @property loginTokens - login tokens
+         * @property {object} loginTokens - login tokens
          */
         RefreshTokenSuccess:"RefreshTokenSuccess",
         /**
          * Invoked when refresh of login tokens failed
-         * @property reason - Failure reason
+         * @property {number} reason - Failure reason
          */
-        RefreshTokenFailed: "RefreshTokenFailed",
+        RefreshTokenFailed: "RefreshTokenFailed"
 
     };
     /**
@@ -437,18 +437,18 @@ function VoxImplantSDK () {
         LogLevelTrace: "trace"
     };
     /**
-     * @property enableVideo - Enable video functionality. Set to true by default. ANDROID ONLY
-     * @property enableHWAcceleration - Enable hardware video acceleration. Set to true by default. Should be set to false, if provideLocalFramesInByteBuffers is set to true. ANDROID ONLY
-     * @property provideLocalFramesInByteBuffers - Request video frames from camera in I420 format with byte buffers. Set to false by default. If set to false, video frames from camera will be provided in I420 format with textures. ANDROID ONLY
-     * @property enableDebugLogging - Enable debug logging. Set to false by default. ANDROID ONLY
+     * @property {boolean} enableVideo - Enable video functionality. Set to true by default. ANDROID ONLY
+     * @property {boolean} enableHWAcceleration - Enable hardware video acceleration. Set to true by default. Should be set to false, if provideLocalFramesInByteBuffers is set to true. ANDROID ONLY
+     * @property {boolean} provideLocalFramesInByteBuffers - Request video frames from camera in I420 format with byte buffers. Set to false by default. If set to false, video frames from camera will be provided in I420 format with textures. ANDROID ONLY
+     * @property {boolean} enableDebugLogging - Enable debug logging. Set to false by default. ANDROID ONLY
      * @property {LogLevel} logLevel - Log levels. IOS ONLY
      */
     this.VoxImplantClientConfig = {
 
     };
     /**
-     * @property connectivityCheck - Checks whether UDP traffic will flow correctly between device and VoxImplant cloud. This check reduces connection speed.
-     * @property servers - Server name of particular media gateway for connection.
+     * @property {boolean} connectivityCheck - Checks whether UDP traffic will flow correctly between device and VoxImplant cloud. This check reduces connection speed.
+     * @property {array} servers - Server name of particular media gateway for connection.
      */
     this.VoxImplantConnectOptions = {
 
